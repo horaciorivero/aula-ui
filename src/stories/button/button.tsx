524c4a3fb2a36a5090afaabb08c7fad8b2a43ee4
horacio rivero <horacio.rivero@educabot.com>
2022-11-30 14:24:22 -0300

    add and try emotion for css styling

--- src/stories/button/button.tsx
+++ src/stories/button/button.tsx
@@ -1,58 +1,75 @@
 import React from 'react'
-// import type { Properties } from 'csstype'
+import { cx, css } from '@emotion/css'
 import type { DetailedHTMLProps, ButtonHTMLAttributes } from 'react'
+
+// TODO
+
+// test tree shaking
+// try override classname and internal styles
+// monorepo?
+// override css property
+// set/upload fonts
+// set global props
+// styled-system
+// border box all
+// set theme
+// create theme hooks
+// create make styles utils, inject theme and props
+// unit tests
+// install bundle size package
 
 export type ButtonSize = 'small' | 'medium' | 'large'
 
-interface StylesProps {
+export interface StylesProps {
   size: ButtonSize
   mode: 'primary' | 'secondary'
   background?: string
 }
 
-// Record<string, Properties<string | number>>
-const useStyles = ({ mode, size, background }: StylesProps) => {
-  const btnSize = {
-    small: {
+const createStyles = ({ background, ...props }: StylesProps) => {
+  // button mode
+  const mode = {
+    primary: css({
+      border: 0,
+      color: 'white',
+      background: '#1ea7fd',
+    }),
+    secondary: css({
+      border: '1px solid #333',
+      color: '#333',
+      background: 'transparent',
+    }),
+  }[props.mode]
+
+  // button size
+  const size = {
+    small: css({
       fontSize: '12px',
       padding: '10px 16px',
-    },
-    medium: {
+    }),
+    medium: css({
       fontSize: '14px',
       padding: '11px 20px',
-    },
-    large: {
+    }),
+    large: css({
       fontSize: '16px',
       padding: '12px 24px;',
-    },
-  }[size]
+    }),
+  }[props.size]
 
-  const btnColor = {
-    primary: {
-      color: 'white',
-      backgroundColor: '#1ea7fd',
+  // button base styles
+  const base = css({
+    fontFamily: "'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif",
+    fontWeight: 700,
+    borderRadius: '4px',
+    cursor: 'pointer',
+    background,
+    '&:hover': {
+      color: 'purple',
     },
-    secondary: {
-      color: 'white',
-      backgroundColor: 'transparent',
-      boxShadow: 'rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset',
-    },
-  }[mode]
+  })
 
-  // border box all
-  return {
-    button: {
-      fontFamily: 'Nunito Sans',
-      fontWeight: 700,
-      border: 0,
-      borderRadius: '3em',
-      cursor: 'pointer',
-      display: 'inline-block',
-      background,
-      ...btnSize,
-      ...btnColor,
-    },
-  }
+  return cx(mode, size, base)
 }
 
 interface ButtonProps
@@ -60,10 +77,12 @@ interface ButtonProps
   primary?: boolean
   background?: string
   size?: ButtonSize
+  className?: string
   children: React.ReactNode
 }
 
 export const Button: React.FC<ButtonProps> = ({
+  className,
   primary = false,
   size = 'medium',
   background,
@@ -71,9 +90,9 @@ export const Button: React.FC<ButtonProps> = ({
   ...props
 }) => {
   const mode = primary ? 'primary' : 'secondary'
-  const styles = useStyles({ size, mode, background })
+  const styles = createStyles({ size, mode, background })
   return (
-    <button style={styles.button} {...props}>
+    <button className={cx(styles, className)} {...props}>
       {children}
     </button>
   )
